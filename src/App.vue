1664d66ba3824e2a95033ef5ec33a254161c6b29
cirodil <cirodil@gmail.com>
2019-08-27 01:02:01 +0300

    Add Toolbar and Drawer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,76 @@
 <template>
-  <h1>Hello Vue</h1>
+
+  <v-app>
+    <v-navigation-drawer
+      app
+      temporary
+      v-model="drawer"
+    >
+      <v-list>
+        <v-list-item
+          v-for="link of links"
+          :key="link.title"
+          :to="link.url"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title v-text="link.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app dark color="primary">
+      <v-app-bar-nav-icon
+        @click="drawer = !drawer" 
+        class="hidden-md-and-up"    
+      >
+      </v-app-bar-nav-icon>
+
+      <v-toolbar-title>Ad application</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn
+          v-for="link in links"
+          :key="link.title"
+          :to="link.url"
+          text
+        >
+          <v-icon left>
+            {{ link.icon }}
+          </v-icon>
+          {{ link.title }}
+        </v-btn>
+      </v-toolbar-items>  
+    </v-app-bar>
+
+    <v-content>
+
+        <router-view></router-view>
+
+    </v-content>
+
+  </v-app>
 </template>
 
 <script>
 
 export default {
+  data () {
+    return {
+      drawer: false,
+      links: [
+        { title: 'Login', icon: 'mdi-lock', url: '/login' },
+        { title: 'Registration', icon: 'mdi-face', url: '/registration' },
+        { title: 'Orders', icon: 'mdi-bookmark-outline', url: '/orders' },
+        { title: 'New ad', icon: 'mdi-file-plus', url: '/new' },
+        { title: 'My ads', icon: 'mdi-format-list-bulleted', url: '/list' }
+      ]
+    }
+  }
 }
 </script>
