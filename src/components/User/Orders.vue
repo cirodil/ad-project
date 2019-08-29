7f4f333a7f5f4c9b1044cd0d312909ff8bf5029b
cirodil <cirodil@gmail.com>
2019-08-29 23:17:45 +0300

    Add orders page

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,19 +1,65 @@
 <template>
-    <v-container>
-        <v-layout row>
-            <v-flex xs12>
-                <h1>Orders</h1>
-            </v-flex>
-        </v-layout>
-    </v-container>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm-3>
+        <h1 class="text--secondary mb-3">Orders</h1>
+        <v-list
+          subheader
+          two-line
+          flat
+        >
+          <v-list-item-group
+          >
+            <v-list-item
+              v-for="order in orders"
+              :key="order.id"
+            >
+              <template>
+                <v-list-item-action>
+                  <v-checkbox
+                    color="primary"
+                    :input-value="order.done"
+                    @change="markDone(order)"
+                  ></v-checkbox>
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title>{{order.name}}</v-list-item-title>
+                  <v-list-item-subtitle>{{order.phone}}</v-list-item-subtitle>
+                </v-list-item-content>
+                <v-list-item-action>
+                  <v-btn
+                    :to="'/ad/' + order.adId"
+                    class="primary"
+                  >Open</v-btn>
+                </v-list-item-action>
+              </template>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-    data () {
-        return {
-            
+  data () {
+    return {
+      orders: [
+        {
+          id: 'fds3',
+          name: 'Ivan',
+          phone: '8-923-332-33-21',
+          adId: '123',
+          done: false
         }
+      ]
     }
+  },
+  methods: {
+    markDone (order) {
+      order.done = true
+    }
+  }
 }
 </script>
