7d2a66cda649d5f35658a6e17a3c42d7c832131f
cirodil <cirodil@gmail.com>
2019-08-31 23:57:52 +0300

    Add vuex

--- src/components/Ads/AdList.vue
+++ src/components/Ads/AdList.vue
@@ -6,7 +6,7 @@
 
         <v-card
           class="elevation-10 mb-5"
-          v-for="ad in ads"
+          v-for="ad in myAds"
           :key="ad.id"
         >
           <v-layout row>
@@ -41,24 +41,9 @@
 
 <script>
 export default {
-  data () {
-    return {
-      ads: [
-        {
-          title: 'First ad',
-          description: 'Hello I am description',
-          promo: false,
-          imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-          id: '123'
-        },
-        {
-          title: 'Second ad',
-          description: 'Hello I am description',
-          promo: true,
-          imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-          id: '1234'
-        }
-      ]
+  computed: {
+    myAds () {
+      return this.$store.getters.myAds
     }
   }
 }
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,7 +5,7 @@
         <v-flex xs12>
           <v-carousel :show-arrows="true">
             <v-carousel-item
-              v-for="ad of ads"
+              v-for="ad of promoAds"
               :key="ad.id"
               :src="ad.imageSrc"
             >
@@ -66,31 +66,12 @@
 
 <script>
 export default {
-  data () {
-    return {
-      ads: [
-        {
-          title: 'First ad',
-          description: 'Hello I am description',
-          promo: false,
-          imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-          id: '123'
-        },
-        {
-          title: 'Second ad',
-          description: 'Hello I am description',
-          promo: true,
-          imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-          id: '1234'
-        },
-        {
-          title: 'Third ad',
-          description: 'Hello I am description',
-          promo: true,
-          imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-          id: '12345'
-        }
-      ]
+  computed: {
+    promoAds () {
+      return this.$store.getters.promoAds
+    },
+    ads () {
+      return this.$store.getters.ads
     }
   }
 }
